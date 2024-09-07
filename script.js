0502ce8c9e53d3d43d72355856e12050def6e248
ANURAG PIPRIYA <anurag.2201230ec@iiitbh.ac.in>
2024-09-07 22:02:09 +0530

    code

---
+++ script.js
@@ -0,0 +1,19 @@
+// document.getElementById('add').addEventListener('click',function(){
+//   let input = document.getElementById('textbox');
+//     let newitem=document.createElement('li');
+//     newitem.textContent=input.value;
+//     document.getElementById('todo').appendChild(newitem);
+//     input.value=''
+// });
+// document.getElementById('del').addEventListener('click',function(){
+//   let input = document.getElementById('todo');
+//   if(input.lastChild){
+//     input.removeChild(input.lastChild);
+//   }
+// });
+
+
+// document.getElementById('delall').addEventListener('click',function(){
+//   let input = document.getElementById('todo');
+//   input.innerHTML=''
+// });
